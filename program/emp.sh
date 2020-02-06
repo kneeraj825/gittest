f67e3fbc1e38ff772adfd046d5602dbbc1e419f1
kneeraj825 <kneeraj825@gmail.com>
2020-02-06 10:58:11 +0530

    Refactor: adding the salary calculation

--- program/emp.sh
+++ program/emp.sh
@@ -2,7 +2,10 @@ present=1
 rn=$((RANDOM%2))
 if [ $present -eq  $rn ]
 then
-echo "employee is present"
+empsal=20
+emphour=8
+empsal=$(($empsal*$emphour))
+echo "employee salry is $empsal"
 else
 echo "employee is not present"
 fi
