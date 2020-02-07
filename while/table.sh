ea82a154c8da2ecac7b772ecd356977702ca2afa
kneeraj825 <kneeraj825@gmail.com>
2020-02-07 15:58:32 +0530

    Refactor: by taking theinput from user

--- while/table.sh
+++ while/table.sh
@@ -1,9 +1,11 @@
 
 #!/bin/bash -x
-
+read -p "enter the number" n
+num=0
 sum=1
-while (( $sum<256 ))
+while (( $num<n ))
 do
-	sum=$(($sum*2))
+	sum=$(( $sum*2 ))
+        (( num++ ))
 	echo $sum
 done
