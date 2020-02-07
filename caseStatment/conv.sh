453ba57419fbce2ad095ee09633ce308ea1d207d
kneeraj825 <kneeraj825@gmail.com>
2020-02-07 10:32:03 +0530

    converstion of feet into meter and meter into inch and viceversa

---
+++ caseStatment/conv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+read -p "enter the number" num1
+read -p "enter the choice" choice
+feettoinch=1
+inchtofeet=2
+feettometer=3
+metertofeet=4
+case "$choice" in
+1)
+num=$(( $num1*12 )) 
+echo " $num inch "
+;;
+2)
+num=`echo "scale=2; $num1/12" |bc`
+echo "$num feet"
+;;
+3)
+num=`echo "scale=2; $num1*0.3" |bc`
+echo " $num meter"
+;;
+4)
+num=`echo "scale=2;  $num1*0.32" |bc` 
+echo "$num feet"
+;;
+*)
+echo "default"
+;;
+esac
