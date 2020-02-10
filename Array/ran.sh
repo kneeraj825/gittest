93eae16d37623460c256e5ff028d942009ed8b3f
kneeraj825 <kneeraj825@gmail.com>
2020-02-10 17:19:25 +0530

    largest and second largest number

--- Array/ran.sh
+++ Array/ran.sh
@@ -7,4 +7,18 @@ do
 done
 echo ${a[@]}
 echo "length of the array is:" ${#a[@]}
-
+max=$(( a[0] ))
+secmax=$(( a[0] ))
+		for(( i=0; i<=10; i++))
+		do
+		if (( $((a[i])) > $max ))
+		then
+		secmax=$max
+		max=$(( a[i] ))
+		elif (( $(( a[i] )) >$secmax && $(( a[i] )) < $max ))  
+		then
+		secmax=$(( a[i]  ))
+		fi
+done
+echo "second largest number :" $max
+echo "second largest number :" $secmax
