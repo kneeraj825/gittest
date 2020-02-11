5a8054234eff56e1c4c4b29f791ac79b280dd47f
kneeraj825 <kneeraj825@gmail.com>
2020-02-11 14:07:40 +0530

    Added:sum of three integer is zero

--- Array/sumint.sh
+++ Array/sumint.sh
@@ -5,21 +5,21 @@ do
 read -p "enter the number in array:" a[i]
 done
 echo ${a[@]}
-	for (( k=0; k<=${#a[@]}; k++ ))
+	 for (( k=0; k<=${#a[@]}; k++ ))
 	do
 	for (( l=k+1; l<=${#a[@]}; l++ ))
 	do
-	for (( m=l+1; m<=${#a[@]}; m++ ))
+			for (( m=l+1; m<=${#a[@]}; m++ ))
 				do
 			res=$(( a[k]+a[l]+a[m] ))
-	done 
-	done
+			done 
+		done
 	done
 		if (( $res==0 ))
 		then
 		echo "$res sum of three integer is zero"
 		else
-		echo " $res sum of three integer is not zero"
+		echo " $res sum of three integers is not zero"
 		fi
 
 
